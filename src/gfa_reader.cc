c3853a71ca55f78443337e4cc38fcaf18afb41fc
xingki641 <xingki641@stu.xjtu.edu.com>
2021-11-02 22:49:13 +0800

    add segmentread

--- src/gfa_reader.cc
+++ src/gfa_reader.cc
@@ -4,6 +4,7 @@
 
 #include "src/gfa_reader.h"
 #include <fstream>
+#include <sstream>
 
 
 namespace gjfish{
@@ -22,30 +23,81 @@ namespace gjfish{
 
         while (getline(file, gfaFileLine)){
             if (gfaFileLine[0] == 'S'){
-                std::cout << gfaFileLine << std::endl;
-                // ReadSeg(gfaFileLine);
+                ReadSeg(gfaFileLine);
             } else if (gfaFileLine[0] == 'L'){
-                std::cout << gfaFileLine << std::endl;
-                // ReadLine(gfaFileLine);
+                ReadLine(gfaFileLine);
             }
         }
     }
-    // S	s105809	AAGGTGCCCGGCACG	LN:i:15	SN:Z:CHM13#LBHZ01000344.1	SO:i:2026584	SR:i:3	cf:f:0.2
-    // L       MTh4001 +       MTh4502 +       0M      SR:i:0
+    // S	s105809	AAGGTGCCCGGCACG	LN:i:15(长度)	SN:Z:CHM13#LBHZ01000344.1(名字）	SO:i:2026584	SR:i:3（	cf:f:0.2
+
 
     void GFAReader::ReadSeg(std::string primitiveSeg) {
         Segment seg = HandlePrimitiveSeg(primitiveSeg);
-        // segments.push_back(seg);
+        segments.push_back(seg);
     }
 
     Segment GFAReader::HandlePrimitiveSeg(std::string primitiveSeg) {
+        Segment seg;
+
+        char delim = '\t';
+        std::istringstream iss(primitiveSeg);
+        std::string tmp;
+        int count = 0;
+        while (std::getline(iss, tmp, delim)){
+            count++;
+            if (count == 3) {
+                seg.seq = tmp;
+            } else if (count > 3) {
+                std::vector<std::string> field = ExtractStringInfo(tmp, ':');
+                if (field[0] == "SN") {
+                    seg.SN = field[2];
+                } else if (field[0] == "SO") {
+                    seg.SO = std::stoi(field[2]);
+                } else if (field[0] == "SR") {
+                    seg.SR = std::stoi(field[2]);
+                }
+            }
+        }
+        return seg;
     }
 
+    // L       MTh4001 +       MTh4502 +       0M      SR:i:0
+    // L	s60647	-	s60646	+	0M(overlap)	SR:i:16	L1:i:1399(长度）	L2:i:350(长度）	cf:f:0.05
     void GFAReader::ReadLine(std::string primitiveLine) {
+        Line line;
+
+        line.leftSegIdx =;
+        char delim = '\t';
+
 
     }
-    void GFAReader::HandlePrimitiveLine() {}
-    void GFAReader::GenerateSuperSeg() {}
+    void GFAReader::HandlePrimitiveLine() {
+
+    }
+    void GFAReader::GenerateSuperSeg() {
+
+    }
+
+    std::vector<std::string> ExtractStringInfo(std::string str, char delim) {
+        std::istringstream iss(str);
+        std::string tmp;
+        std::vector<std::string> splitStr;
+        while (std::getline(iss, tmp, delim)) {
+            splitStr.push_back(tmp);
+        }
+        return splitStr;
+    }
+
+    std::string ReverseComplement(std::string sequence) {
+        int n = sequence.size();
+        std::string res(n, 'x');
+        for (int i = n - 1; i >= 0; i--) {
+            res[n - i - 1] = REVERSE_TABLE[sequence[i]];
+        }
+        return res;
+    }
+
 }
 int main()
 {
