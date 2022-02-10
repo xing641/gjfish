52aa96d458746ac066175ca03be648e6a105cdf2
xingki641 <624528136@qq.com>
2022-02-10 16:23:43 +0800

    test

--- src/test.cc
+++ src/test.cc
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
     param.kmer_width = 1;
     param.k = 28;
     param.threads_count = 16;
-    param.mem_size = 16000000000;
+    param.mem_size = 20000000000;
     param.result_site_dir = "kmer_site_out_file_name.bin";
     param.input_file = argv[1];
     // param.input_file = "../test/KmerCounter_test.gfa";
